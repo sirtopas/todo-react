b5bd9f7734f608c9101e26129a84701211e4d48a
James <sirtopas@gmail.com>
2019-08-21 22:02:47 +0100

    Added methods to submit a simple test form
    Signed-off-by: James <sirtopas@gmail.com>

--- todo-app-client/src/App.js
+++ todo-app-client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
             <Router>
                 <div className="container">
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#" target="_blank">
+                        <a className="navbar-brand" href="#" target="_blank">
                             <img src={logo} width="30" height="30" />
                         </a>
                         <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
