04164da05d430f7e517014f8b1a6a6c8f525ddf4
James <sirtopas@gmail.com>
2019-08-23 13:21:15 +0100

    Added api call to get todo list on todolist component
    Signed-off-by: James <sirtopas@gmail.com>

--- todo-app-server/server.js
+++ todo-app-server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000; app.use(cors());
 const todoRoutes = express.Router();
-app.use(bodyParser.json()); mongoose.connect('mongodb+srv://cronus:[password]@cluster0-ysquu.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+app.use(bodyParser.json()); mongoose.connect('mongodb+srv://cronus:Yk^eVfCwhn3qRWCd[nVcTYq3@cluster0-ysquu.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 const connection = mongoose.connection; connection.once('open', function () {
     console.log("MongoDB database connection established successfully");
 });
